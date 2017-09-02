b852ca465792f6fced781252c10801c2c569865b
“belgradebotnet” <26587568+belgradebotnet@users.noreply.github.com>
2017-09-02 22:30:30 +1000

    Added imports for generators and types

---
+++ main.nim
@@ -0,0 +1,14 @@
+# Import the required modules
+import os, random
+
+# import the generator modules
+import types
+import simplequadratic
+import advancedquadratic
+
+# set the random number generator to something unique
+randomize()
+
+# test generators
+echo simplequadratic.generator()
+echo advancedquadratic.generator()
