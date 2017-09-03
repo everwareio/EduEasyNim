3875ed665ce0540314e6e3ea404122167b10f2a9
“belgradebotnet” <26587568+belgradebotnet@users.noreply.github.com>
2017-09-03 15:15:47 +1000

    Now generates questions based on given command line input

--- main.nim
+++ main.nim
@@ -1,5 +1,5 @@
 # Import the required modules
-import os, random
+import os, random, types, strutils
 
 # import the generator modules, the generator folder path is passed to the
 # compiler with the command
@@ -11,8 +11,29 @@ import differentiation.constantdifferentiation
 # set the random number generator to something unique
 randomize()
 
-# test generators
-echo monicquadratic.generator()
-echo nonmonicquadratic.generator()
-echo singletermdifferentiation.generator()
-echo constantdifferentiation.generator()
+# open the files
+var worksheet: File = open("questions.txt", fmAppend)
+var solutions: File = open("solutions.txt", fmAppend)
+
+# write the title
+write(worksheet, paramStr(1) & "\n\n")
+write(solutions, paramStr(1) & " (Solutions)\n\n")
+
+# loop to create the specified amount of questions
+var thisq: Question
+for i in countup(1, parseInt(paramStr(3))):
+
+  # figure out what questions have been requested
+  case paramStr(2)
+  of "monic": thisq = monicquadratic.generator()
+  of "nonmonic": thisq = nonmonicquadratic.generator()
+  of "singleterm": thisq = singletermdifferentiation.generator()
+  of "constant": thisq = constantdifferentiation.generator()
+
+  # write the question to the file
+  write(worksheet, $i & ") " & thisq.question & "\n")
+  write(solutions, $i & ") " & thisq.solution & "\n")
+
+# close the files
+close(worksheet)
+close(solutions)
